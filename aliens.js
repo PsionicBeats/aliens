3d8abf313be3facc8a9ec228885d76bb34778fd6
Alex Aguinaldo <psionicbones@gmail.com>
2016-11-02 01:25:28 -1000

    Update

---
+++ aliens.js
@@ -0,0 +1,13 @@
+var green = 5001
+var red = 2999
+var blue = 2000
+
+if (green = <5000) {
+	return console.log ("Ugh, why are they so ugly?")
+}
+if (green + red = <8000) {
+	return console.log ("No, why?????")
+}
+if (red + blue = >3000) {
+	return console.log ("Okay, this seems somewhat manageable. Wait no, we're still dead...")
+}
